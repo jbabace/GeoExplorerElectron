720604479d0043e54416bbe6da8ec70d4f6be1c0
2dam3 <2dam3@HZ114488.H012108.NET>
2021-01-08 10:32:44 +0100

    chat Electron

---
+++ src/main.js
@@ -0,0 +1,22 @@
+const { BrowserWindow } = require("electron");
+const path = require('path')
+
+
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron.cmd')
+})
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 2000,
+    height: 1400,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  win.maximize();
+
+  win.loadFile("src/index.html");
+}
+
+module.exports = { createWindow };
