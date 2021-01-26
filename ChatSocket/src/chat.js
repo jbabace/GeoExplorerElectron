f37cbad056e9bf22619bbf66a4e7863fff9d1fdd
2dam3 <2dam3@HZ114488.H012108.NET>
2021-01-26 12:55:59 +0100

    Cambios

--- ChatSocket/src/chat.js
+++ ChatSocket/src/chat.js
@@ -1,31 +1,36 @@
 var net = require('net');
+var usuario = "Endibra";
+var ruta = "Ruta X";
 
 var client = new net.Socket();
 client.connect(1234, '127.0.0.1', function() {
-    console.log('Connected');
-    var loginJson = '{ "action": "login", "user":"Endibra", "route":"Ruta X"}';
+    console.log('Conectado');
+    var loginJson = '{ "action": "login", "user":"'+usuario+'", "route":"'+ruta+'"}';
     
     client.write(loginJson);
-    client.end();
+    client.write("\n");
 });
 
 
 client.on("data", (data) => {
-    console.log(data.toString());
-    client.end();
+    var datos = JSON.parse(data);
+    anadirTextoExterno(datos);
 });
 
-function escribirSocket() {
-    console.log("Vamos a conectarnos");
-    
-    
+function escribirTextoInterno(mensaje) {
+    var mensajeJSON = '{"action":"msg","from":"'+usuario+'","route":"'+ruta+'","value":"'+mensaje+'"}';
+    client.write(mensajeJSON);
+    client.write("\n");
 }
+
 
 function botonPulsado() {
 
+    //si se le da enter, envia el texto automaticamente
     if (window.event.keyCode === 13) {
         anadirTexto();
         
+        //vaciamos el textarea y le ajustamos el tamaño en caso de que haya escrito mucho
         $(".textareaChat").val("");
         $(".textareaChat").height('5px');
 
@@ -35,7 +40,9 @@ function botonPulsado() {
 }
 
 function anadirTexto() {
-    if($('.textareaChat').val() != ""){
+    mensaje = $('.textareaChat').val();
+    if(mensaje != ""){
+        escribirTextoInterno(mensaje);
         var content = document.createElement("div");
 
         //cambiamos
@@ -48,6 +55,20 @@ function anadirTexto() {
         //vaciamos el textarea
         $(".textareaChat").val("");
         $(".textareaChat").height('5px');
+    }
+}
+
+function anadirTextoExterno(json) {
+    if(json["value"] != ""){
+        var content = document.createElement("div");
+
+        //cambiamos
+        mensaje = json["value"];
+        var html = mensaje.replace(/(\n|\r|\r\n)/g, '<br>');
+
+        content.innerHTML = html;
+    
+        $('#contenidoChat').append(content).append('<hr>');
     }
 }
 
